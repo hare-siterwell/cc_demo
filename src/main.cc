2e5d2da95e0d35a551ee24226ba9a912714596cd
Hare <renilr0@outlook.com>
2022-03-04 11:30:32 +0800

    first commit

---
+++ src/main.cc
@@ -0,0 +1,12 @@
+/**
+ * @file main.cc
+ * @brief Main program body
+ */
+#include <iostream>
+
+/**
+ * @brief The application entry point
+ *
+ * @return int
+ */
+auto main() -> int { std::cout << "Hello world!" << std::endl; }

8743366be86c9973f0476eae0442cd4c29a47ac1
Hare <renilr0@outlook.com>
2022-10-15 11:15:44 +0800

    refactor: Formatting

--- src/main.cc
+++ src/main.cc
@@ -4,18 +4,13 @@
  * @brief Main program body
  * @version 0.1
  * @date 2022-07-07
- *
  * @copyright Copyright (c) 2022
- *
  */
 
 #include "main.h"
 
-#include <iostream>
-
 /**
  * @brief The application entry point
- *
  * @return int
  */
 int main() { std::cout << "Hello World" << std::endl; }
